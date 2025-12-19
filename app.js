94770a2415b2dbd8732aa06e9f547ff673d3cbfd
Hassan-Ali0 <hassanali031935@gmail.com>
2025-12-19 16:33:12 +0500

    fix: add trailing slash to CORS origin URL in app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: 'https://snap-talk-web.netlify.app',
+        origin: 'https://snap-talk-web.netlify.app/',
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
         credentials: true
     }
