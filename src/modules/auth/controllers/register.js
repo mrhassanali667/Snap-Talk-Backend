c3ab05f4cd5032b59d5fb9ad488528a810a899ca
Hassan-Ali0 <hassanali031935@gmail.com>
2025-12-20 18:55:12 +0500

    fix: update cookie path in login and register controllers to root

--- src/modules/auth/controllers/register.js
+++ src/modules/auth/controllers/register.js
@@ -8,7 +8,7 @@ const registerController = async (req, res) => {
             httpOnly: true,
             secure: true,
             sameSite: 'none',
-            path: '/api',
+            path: '/',
             domain: 'snap-talk-web.netlify.app',
             maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
         })
